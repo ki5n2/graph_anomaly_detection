4e28af233f81cd61ea30f2ce56fe7b8087a4de06
ki5n2 <rldnjs5n2@gmail.com>
2024-10-02 00:54:06 +0900

    code upt

--- util.py
+++ util.py
@@ -13,6 +13,7 @@ from torch_geometric.transforms import Constant
 from sklearn.model_selection import StratifiedKFold
 from torch_geometric.utils import to_dense_adj, to_undirected, to_networkx
 
+import networkx as nx
 
 def set_seed(random_seed):
     torch.manual_seed(random_seed)
@@ -305,5 +306,127 @@ def get_data_loaders_TU(dataset_name, batch_size, test_batch_size, split, datase
     loader_dict = {'train': dataloader, 'test': dataloader_test}
 
     return loader_dict, meta
+
+
+# %%
+def read_graph_file(dataset_name, path):
+    prefix = os.path.join(path, dataset_name, 'raw', dataset_name)
+    filename_graph_indic = prefix + '_graph_indicator.txt'
+    graph_indic = {}
+    with open(filename_graph_indic) as f:
+        i = 1
+        for line in f:
+            line = line.strip("\n")
+            graph_indic[i] = int(line)
+            i += 1
+
+    filename_nodes = prefix + '_node_labels.txt'
+    node_labels = []
+    try:
+        with open(filename_nodes) as f:
+            for line in f:
+                line = line.strip("\n")
+                node_labels += [int(line) - 1]
+        num_unique_node_labels = max(node_labels) + 1
+    except IOError:
+        print('No node labels')
+
+    filename_node_attrs = prefix + '_node_attributes.txt'
+    node_attrs = []
+    try:
+        with open(filename_node_attrs) as f:
+            for line in f:
+                line = line.strip("\s\n")
+                attrs = [float(attr) for attr in re.split("[,\s]+", line) if not attr == '']
+                node_attrs.append(np.array(attrs))
+    except IOError:
+        print('No node attributes')
+
+    label_has_zero = False
+    filename_graphs = prefix + '_graph_labels.txt'
+    graph_labels = []
+
+    label_vals = []
+    with open(filename_graphs) as f:
+        for line in f:
+            line = line.strip("\n")
+            val = int(line)
+            if val not in label_vals:
+                label_vals.append(val)
+            graph_labels.append(val)
+
+    label_map_to_int = {val: i for i, val in enumerate(label_vals)}
+    graph_labels = np.array([label_map_to_int[l] for l in graph_labels])
+
+    filename_adj = prefix + '_A.txt'
+    adj_list = {i: [] for i in range(1, len(graph_labels) + 1)}
+    index_graph = {i: [] for i in range(1, len(graph_labels) + 1)}
+    num_edges = 0
+    with open(filename_adj) as f:
+        for line in f:
+            line = line.strip("\n").split(",")
+            e0, e1 = (int(line[0].strip(" ")), int(line[1].strip(" ")))
+            adj_list[graph_indic[e0]].append((e0, e1))
+            index_graph[graph_indic[e0]] += [e0, e1]
+            num_edges += 1
+    for k in index_graph.keys():
+        index_graph[k] = [u - 1 for u in set(index_graph[k])]
+
+    graphs = []
+    for i in range(1, 1 + len(adj_list)):
+        G = nx.from_edgelist(adj_list[i])
+        G.graph['label'] = graph_labels[i - 1]
+
+        mapping = {}
+        it = 0
+        for n in G.nodes:
+            mapping[n] = it
+            it += 1
+
+        G_pyg = from_networkx(nx.relabel_nodes(G, mapping))
+        G_pyg.y = G.graph['label']
+        G_pyg.x = torch.ones((G_pyg.num_nodes,1))
+
+        if G_pyg.num_nodes > 0:
+            graphs.append(G_pyg)
+
+    return graphs
+
+
+def get_ad_dataset_Tox21(dataset_name, batch_size, test_batch_size, need_str_enc=False):
+    path = osp.join(osp.dirname(osp.realpath(__file__)), 'dataset')
+
+    data_train_ = read_graph_file(dataset_name + '_training', path)
+    data_test = read_graph_file(dataset_name + '_testing', path)
+
+    dataset_num_features = data_train_[0].num_features
+
+    data_train = []
+    for data in data_train_:
+        if data.y == 1:
+            data_train.append(data)
+
+    idx = 0
+    for data in data_train:
+        data.y = 0
+        data['idx'] = idx
+        idx += 1
+
+    for data in data_test:
+        data.y = 1 if data.y == 1 else 0
+    
+    data_ = data_train + data_test
+    max_nodes = max([data_[i].num_nodes for i in range(len(data_))])
+    
+    # if need_str_enc:
+    #     data_train = init_structural_encoding(data_train, rw_dim=args.rw_dim, dg_dim=args.dg_dim)
+    #     data_test = init_structural_encoding(data_test, rw_dim=args.rw_dim, dg_dim=args.dg_dim)
+
+    dataloader = DataLoader(data_train, batch_size=batch_size, shuffle=True)
+    dataloader_test = DataLoader(data_test, batch_size=test_batch_size, shuffle=True)
+    meta = {'num_feat':dataset_num_features, 'num_train':len(data_train), 'max_nodes': max_nodes}
+
+    return dataloader, dataloader_test, meta
+
 
 # %%
